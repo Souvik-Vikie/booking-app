7c2038f918c13926fdcf7572b31fe9990570518a
Souvik-Vikie <souvikkarmakar368@gmail.com>
2025-09-24 02:42:39 +0530

    Base url updated

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://souvik-booking-app-backend.onrender.com/api'
-  : 'http://localhost:8800/api';
+const API_BASE_URL =  'https://souvik-booking-app-backend.onrender.com/api'
+  
+// const API_BASE_URL = 'http://localhost:8800/api';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
